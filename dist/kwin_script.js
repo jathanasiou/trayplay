4d376f78daa168f0501b2f544ca15fc69a4aade2
kabuspl <me@kabus.ovh>
2025-12-15 17:52:06 +0100

    better fullscreen window detection

--- dist/kwin_script.js
+++ dist/kwin_script.js
@@ -1,13 +1,35 @@
+var active_window = null;
+
 function send(window) {
+    if (window.active) {
+        callDBus(
+            "ovh.kabus.trayplay",
+            "/ovh/kabus/trayplay",
+            "ovh.kabus.trayplay.ActiveWindowManager",
+            "SetActiveWindow",
+            window.desktopFileName,
+            window.caption,
+            window.fullScreen,
+        );
+        if (active_window != null) {
+            active_window.fullScreenChanged.disconnect(fullScreenChanged);
+        }
+        active_window = window;
+        active_window.fullScreenChanged.connect(fullScreenChanged);
+    }
+}
+
+function fullScreenChanged() {
     callDBus(
         "ovh.kabus.trayplay",
         "/ovh/kabus/trayplay",
         "ovh.kabus.trayplay.ActiveWindowManager",
         "SetActiveWindow",
-        window.desktopFileName,
-        window.caption,
-        window.fullScreen,
+        active_window.desktopFileName,
+        active_window.caption,
+        active_window.fullScreen,
     );
 }
 
 workspace.windowActivated.connect(send);
+workspace.windowAdded.connect(send);
